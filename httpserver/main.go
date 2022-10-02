2e87872484e813d2ff79a46b75c88460ed87ce66
sefatanam <sefatanam@gmail.com>
2022-10-03 01:06:07 +0600

    made changes in httpserver

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"os"
 )
@@ -12,7 +14,12 @@ const keyServerAddr = "serverAddr"
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Printf("got /request\n")
+	ctx := r.Context()
+
+	hasFirst := r.URL.Query().Has("first")
+	first := r.URL.Query().Get("first")
+
+	fmt.Printf("%s got /request. first(%t)=%s \n", ctx.Value(keyServerAddr), hasFirst, first)
 	_, err := io.WriteString(w, "This is my website.\n")
 	if err != nil {
 		return
@@ -21,7 +28,8 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 
 func getHello(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Printf("got hello /request\n")
+	ctx := r.Context()
+	fmt.Printf("%s got hello /request\n", ctx.Value(keyServerAddr))
 	_, err := io.WriteString(w, "Hello HTTP!\n")
 
 	if err != nil {
@@ -35,6 +43,48 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", getRoot)
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	serverOne := &http.Server{
+		Addr:    ":3001",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	serverTwo := &http.Server{
+		Addr:    ":3003",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server one closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server one: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	go func() {
+		err := serverTwo.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server two closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server two: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	<-ctx.Done()
+
 	mux.HandleFunc("/hello", getHello)
 
 	err := http.ListenAndServe(":3000", mux)
