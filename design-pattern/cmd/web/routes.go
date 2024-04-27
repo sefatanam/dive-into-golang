db2fb5ec5a659f574bb7f905986b628664489b4f
sefatanam <sefatanam@gmail.com>
2024-04-27 15:10:36 +0600

    udemy(design-pattern): first web application

---
+++ design-pattern/cmd/web/routes.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// Using middleware
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Timeout(30 * time.Second))
+
+	// Using Routes
+	mux.Get("/", app.ShowHome)
+	return mux
+}
---
+++ design-pattern/go.mod
@@ -0,0 +1,5 @@
+module design-pattern
+
+go 1.22.2
+
+require github.com/go-chi/chi/v5 v5.0.12 // indirect
