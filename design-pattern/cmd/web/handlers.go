3344e204496ae4e579663fcc905c7bee98a6fca0
sefatanam <sefatanam@gmail.com>
2024-04-29 00:27:07 +0600

    create two routes

--- design-pattern/cmd/web/handlers.go
+++ design-pattern/cmd/web/handlers.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"design-pattern/pets"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/tsawler/toolbox"
 )
 
 func (app *application) ShowHome(w http.ResponseWriter, r *http.Request) {
@@ -14,4 +16,14 @@ func (app *application) ShowHome(w http.ResponseWriter, r *http.Request) {
 func (app *application) ShowPage(w http.ResponseWriter, r *http.Request) {
 	page := chi.URLParam(r, "page")
 	app.render(w, fmt.Sprintf("%s.page.gohtml", page), nil)
+}
+
+func (app *application) CreateDogFromFactory(w http.ResponseWriter, r *http.Request) {
+	var t toolbox.Tools
+	_ = t.WriteJSON(w, http.StatusOK, pets.NewPet("dog"))
+}
+
+func (app *application) CreateCatFromFactory(w http.ResponseWriter, r *http.Request) {
+	var t toolbox.Tools
+	_ = t.WriteJSON(w, http.StatusOK, pets.NewPet("cat"))
 }
--- design-pattern/go.mod
+++ design-pattern/go.mod
@@ -2,4 +2,7 @@ module design-pattern
 
 go 1.22.2
 
-require github.com/go-chi/chi/v5 v5.0.12 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/tsawler/toolbox v1.3.1 // indirect
+)
