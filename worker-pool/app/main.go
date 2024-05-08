1f86239abd2ba7fb99aab57db6bed7485eee3b66
sefatanam <sefatanam@gmail.com>
2024-05-08 16:39:28 +0600

    basic worker pool structure implemented

---
+++ worker-pool/app/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"streamer"
+)
+
+func main() {
+	// define number and jobs
+	const numJobs = 4
+	const numWorkers = 2
+
+	// create channel for works and results
+	notifyChan := make(chan streamer.ProcessingMessage, numJobs)
+	defer close(notifyChan)
+
+	videoQueue := make(chan streamer.VideoProcessingJob, numJobs)
+	defer close(videoQueue)
+
+	// Get a worker pool.
+	wp := streamer.New(videoQueue, numWorkers)
+	fmt.Println(wp)
+	// Start the worker pool.
+
+	// Create 4 videos to send to the worker pool
+
+	// Send videos to the worker pool.
+
+	// Print out results.
+
+}
