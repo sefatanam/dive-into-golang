4998e270a75f325e344a0220a044d914d8736043
sefatanam <sefatanam@gmail.com>
2024-10-13 23:54:19 +0600

    add todo save fn

--- task-cli/main.go
+++ task-cli/main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 	"sync"
 	"time"
 )
-
-type Task struct {
-	Id          int       `json:'id'`
-	Description string    `json:'description'`
-	Status      string    `json:'status'`
-	CreatedAt   time.Time `json:'created_at'`
-	UpdatedAt   time.Time `json:'updated_at'`
-}
 
 // global variables
 const (
@@ -22,41 +13,6 @@ const (
 )
 
 var mutex sync.Mutex
-
-func loadTasks(fileName string) ([]Task, error) {
-	if _, err := os.Stat(fileName); os.IsNotExist(err) {
-		return []Task{}, err
-	}
-
-	data, err := os.ReadFile(fileName)
-	if err != nil {
-		return []Task{}, err
-	}
-	var tasks []Task
-
-	err = json.Unmarshal(data, &tasks)
-	return tasks, err
-}
-
-// Helper function to list all tasks
-func listTasks(tasks []Task) {
-	fmt.Println("ID   | Task Description               | Status      | Created At           | Updated At")
-	fmt.Println("-----|--------------------------------|-------------|----------------------|----------------------")
-	for _, task := range tasks {
-		fmt.Printf("%-4d | %-30s | %-11s | %-20s | %-20s\n", task.Id, task.Description, task.Status, task.CreatedAt.Format("2006-01-02 15:04:05"), task.UpdatedAt.Format("2006-01-02 15:04:05"))
-	}
-}
-
-// Helper function to list tasks by status
-func listTasksByStatus(tasks []Task, status string) {
-	fmt.Println("ID   | Task Description               | Status      | Created At           | Updated At")
-	fmt.Println("-----|--------------------------------|-------------|----------------------|----------------------")
-	for _, task := range tasks {
-		if task.Status == status {
-			fmt.Printf("%-4d | %-30s | %-11s | %-20s | %-20s\n", task.Id, task.Description, task.Status, task.CreatedAt.Format("2006-01-02 15:04:05"), task.UpdatedAt.Format("2006-01-02 15:04:05"))
-		}
-	}
-}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -66,7 +22,7 @@ func main() {
 
 	command := os.Args[1]
 
-	tasks, err := loadTasks(fileName)
+	tasks, err := LoadTasks(fileName)
 
 	if err != nil {
 		fmt.Println("Error loading tasks: ", err)
@@ -77,10 +33,30 @@ func main() {
 	case "list":
 		if len(os.Args) > 2 {
 			status := os.Args[2]
-			listTasksByStatus(tasks, status)
+			ListTasksByStatus(tasks, status)
 		} else {
-			listTasks(tasks)
+			ListTasks(tasks)
 		}
+	case "add":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: task-cli add [task description]")
+			return
+		}
+
+		description := os.Args[2]
+		id := len(tasks) + 1
+
+		task := Task{
+			Id:          id,
+			Description: description,
+			Status:      "todo",
+			CreatedAt:   time.Now(),
+			UpdatedAt:   time.Now(),
+		}
+
+		tasks = append(tasks, task)
+		SaveTask(tasks, fileName)
+		fmt.Println("Task added successfully (Id:", id, ")")
 
 	default:
 		fmt.Println("Unknown command:", command)
